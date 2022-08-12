862260d54de71addc9e96b153cd3f83a81a72d8a
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-08-12 14:21:14 -0700

    Category models done. Just need to do the other models and do routes

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
@@ -13,3 +12,17 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+ 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// connection.connect(function (err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId + "\n");
+
+// });
